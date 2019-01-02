a0df18475bf5bde6619e21f904b967baaf033ca4
del77 <xdelpl@gmail.com>
2019-01-02 17:15:40 +0100

    db serializer, tests

--- DatabaseSerializer/DatabaseSerializer.cs
+++ DatabaseSerializer/DatabaseSerializer.cs
@@ -11,6 +11,84 @@ namespace DatabaseSerializer
     [Export(typeof(ISerializer))]
     public class DatabaseSerializer : ISerializer
     {
-        
+        private DatabaseSerializerContext context;
+
+        public DatabaseSerializer()
+        {
+            context = new DatabaseSerializerContext();
+            Mapper.Initialize(cfg =>
+            {
+                cfg.CreateMap<AssemblyDto, AssemblyDb>().ForMember(dest => dest.AssemblyDbId, opt => opt.Ignore());
+                cfg.CreateMap<FieldDto, FieldDb>().ForMember(dest => dest.FieldDbId, opt => opt.Ignore());
+                cfg.CreateMap<MethodDto, MethodDb>().ForMember(dest => dest.MethodDbId, opt => opt.Ignore());
+                cfg.CreateMap<NamespaceDto, NamespaceDb>().ForMember(dest => dest.NamespaceDbId, opt => opt.Ignore());
+                cfg.CreateMap<PropertyDto, PropertyDb>().ForMember(dest => dest.PropertyDbId, opt => opt.Ignore());
+                cfg.CreateMap<TypeDto, TypeDb>().ForMember(dest => dest.TypeDbId, opt => opt.Ignore());
+            });
+        }
+
+        public void Serialize(object model)
+        {
+            context.Assemblies.RemoveRange(context.Assemblies);
+            context.Fields.RemoveRange(context.Fields);
+            context.Methods.RemoveRange(context.Methods);
+            context.Namespaces.RemoveRange(context.Namespaces);
+            context.Properties.RemoveRange(context.Properties);
+            context.Types.RemoveRange(context.Types);
+
+            AssemblyDto assemblyDto = (AssemblyDto)model;
+            AssemblyDb assemblyDb = Mapper.Map<AssemblyDb>(assemblyDto);
+
+            context.Assemblies.Add(assemblyDb);
+            context.SaveChanges();
+        }
+
+        public object Deserialize()
+        {
+            context.Namespaces
+                .Include(x => x.Namespaces)
+                .Include(x => x.Types)
+                .Load();
+
+            context.Assemblies
+                .Include(x => x.Namespaces)
+                .Load();
+
+            context.Fields
+                .Include(x => x.Attributes)
+                .Include(x => x.TypeModel)
+                .Load();
+
+            context.Methods
+                .Include(x => x.Attributes)
+                .Include(x => x.GenericArguments)
+                .Include(x => x.Parameters)
+                .Include(x => x.ReturnType)
+                .Load();
+
+            context.Properties
+                .Include(x => x.Attributes)
+                .Include(x => x.Getter)
+                .Include(x => x.Setter)
+                .Include(x => x.Type)
+                .Load();
+
+            context.Types
+                .Include(x => x.Attributes)
+                .Include(x => x.BaseType)
+                .Include(x => x.Constructors)
+                .Include(x => x.DeclaringType)
+                .Include(x => x.Fields)
+                .Include(x => x.GenericArguments)
+                .Include(x => x.ImplementedInterfaces)
+                .Include(x => x.Methods)
+                .Include(x => x.FieldType)
+                .Include(x => x.NestedTypes)
+                .Load();
+
+            AssemblyDb ret = context.Assemblies.FirstOrDefault();
+
+            return Mapper.Map<AssemblyDto>(ret);
+        }
     }
 }
--- FileTests/FileSerializerTests.cs
+++ FileTests/FileSerializerTests.cs
@@ -13,6 +13,47 @@ namespace FileTests
     [TestClass]
     public class FileSerializerTests
     {
-        
+        private static string path = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\test.dll"));
+        private static ModelService modelService = new ModelService();
+        private static AssemblyModel deserialized;
+        private static AssemblyModel reflectedModel;
+
+        [ClassInitialize]
+        public static void Init(TestContext testContext)
+        {
+            AggregateCatalog catalog = new AggregateCatalog();
+            catalog.Catalogs.Add(new AssemblyCatalog(typeof(FileSerializer.FileSerializer).Assembly));
+            CompositionContainer container = new CompositionContainer(catalog);
+            try
+            {
+                container.ComposeParts(modelService);
+                IReflector reflector = new Reflector();
+                reflector.Reflect(path);
+                reflectedModel = reflector.AssemblyModel;
+                modelService.Save(reflectedModel);
+                deserialized = modelService.Load();
+            }
+            catch (CompositionException compositionException)
+            {
+                Console.WriteLine(compositionException.ToString());
+            }
+        }
+
+        [TestMethod]
+        public void Deserialized_Model_Shouldnt_Create_New_Objects_For_Same_Types()
+        {
+            TypeModel serviceB = deserialized.Namespaces[2].Types[1];
+            TypeModel serviceB_ref = deserialized.Namespaces[2].Types[2].Fields[0].TypeModel;
+
+            Assert.AreSame(serviceB_ref, serviceB);
+        }
+
+        [TestMethod]
+        public void Check_If_Model_Is_valid()
+        {
+            Assert.AreEqual(reflectedModel.Name, deserialized.Name);
+            Assert.AreEqual(reflectedModel.Namespaces.Count, deserialized.Namespaces.Count);
+            Assert.AreEqual(reflectedModel.Namespaces.Sum(x => x.Types.Count), deserialized.Namespaces.Sum(x => x.Types.Count));
+        }
     }
 }
